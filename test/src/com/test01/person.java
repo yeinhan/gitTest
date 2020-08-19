e2d0f01a87974b2f88a393ad45de84d46b1d369f
=yeinhan <goregore8637@naver.com>
2020-08-19 21:11:50 +0900

    no message

---
+++ test/src/com/test01/person.java
@@ -0,0 +1,17 @@
+package com.test01;
+
+public class person {
+	private int num;
+	private String name;
+	public person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public person(int num, String name) {
+		super();
+		this.num = num;
+		this.name = name;
+	}
+	
+	
+}
---
+++ test/src/com/test01/test01.java
@@ -0,0 +1,7 @@
+package com.test01;
+
+public class test01 {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
